9b58bdcdeafd94747d534e0afeb6116210b84ddb
19875196595 <2458190458@qq.com>
2024-03-18 15:35:31 +0800

    test

---
+++ client/xx.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: 19875196595 2458190458@qq.com
+ * @Date: 2024-03-18 15:35:06
+ * @LastEditors: 19875196595 2458190458@qq.com
+ * @LastEditTime: 2024-03-18 15:35:16
+ * @FilePath: \socialnexus\client\xx.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
