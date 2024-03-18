a6fe6aeb3555660968299a7acd13500e0e6c78fe
19875196595 <2458190458@qq.com>
2024-03-18 21:03:42 +0800

    postcss

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 19875196595 2458190458@qq.com
+ * @Date: 2024-03-18 20:25:27
+ * @LastEditors: 19875196595 2458190458@qq.com
+ * @LastEditTime: 2024-03-18 20:41:57
+ * @FilePath: \socialnexus\client\postcss.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const path = require('path')
 const webpack = require('webpack')
 const config = {
