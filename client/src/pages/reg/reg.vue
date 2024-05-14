023480bc44892ae1860f769c98dbb90a204bc554
happy <2630391116@qq.com>
2024-05-14 15:14:15 +0800

    注册-上传图片解决上传前后大小间距问题，切换角色后图片大小间距-陈卓敏

--- client/src/pages/reg/reg.vue
+++ client/src/pages/reg/reg.vue
@@ -5,19 +5,20 @@
 		<!-- 切换身份按钮 -->
 		<view style="padding: 30rpx;">
 			<!-- 由于应用场景可能存在边距，所有组件中没有默认设置边距，可以通过父元素来控制 -->
-			<YtabBtns :data="list" :index.sync="index"></YtabBtns>
+			<YtabBtns :data="list" :index.sync="index" @change="change" ></YtabBtns>
 		</view>
 
-    	<!-- <image class="banner" src="../../static/logo.png"></image> -->
-		<u-upload class="banner" @on-success="handleSuccess" :custom-btn="true" max-count="4" ref="uUpload" :action="action" :file-list="comModal.comImgs" :auto-upload="false">
-			<view slot="addBtn" class="slot-btn" hover-class="slot-btn__hover" hover-stay-time="150">
-				<image class="banner" src="http://scu5azomr.hn-bkt.clouddn.com/static/icon/upload.png"></image>
-				<view style="font-size: 30rpx;">上传头像</view>
-			</view>
-		</u-upload>
+    	<view class="container">
+			<u-upload :class="{ 'banner': avatar === '' }" @on-success="handleSuccess" @on-remove="handleRemove" :custom-btn="true" :max-count="1" ref="uUpload" 
+						:action="action" :file-list="avatarList" :auto-upload="true" style="display: flex; align-items: center; ">
+				<view slot="addBtn" class="slot-btn" hover-class="slot-btn__hover" hover-stay-time="150">
+					<image :class="{ 'banner': avatar === '' } " src="http://scu5azomr.hn-bkt.clouddn.com/static/icon/upload.png"></image>
+				</view>
+			</u-upload>
+		</view>
 
 			<!-- 高校队伍进行注册 -->
-			<view class="input-group" v-if="index == 0">
+			<view class="input-group" style="margin-top: 60rpx;" v-if="index == 0">
 				<view class="input-row border">
 					<text class="title">队长ID：</text>
 					<m-input type="text" focus clearable v-model="account" placeholder="请输入队长ID"></m-input>
@@ -41,7 +42,7 @@
 			</view>
 
 			<!-- 社区基层人员进行注册 -->
-			<view class="input-group" v-else>
+			<view class="input-group" style="margin-top: 60rpx;" v-else>
 				<view class="input-row border">
 					<text class="title">账号：</text>
 					<m-input type="text" focus clearable v-model="account" placeholder="请输入账号"></m-input>
@@ -90,7 +91,6 @@
 </template>
 
 <script>
-	import service from '../../service.js';
 	import mInput from '../../components/m-input.vue';
 	import YtabBtns from '../../components/login-tab/YtabBtns.vue'
 	import cityPicker from '@/uni_modules/piaoyi-cityPicker/components/piaoyi-cityPicker/piaoyi-cityPicker'
@@ -117,7 +117,7 @@
 					}
 				],
 
-				identity: '', //指示高校/社区
+				// identity: '', //指示高校/社区
 				// 高校队伍注册需要的信息
 				account: '',
 				password: '',
@@ -127,6 +127,11 @@
 				remark: '',
 				leader_id: '',
 				avatar:'',
+				avatarList: [
+					// {
+					// 	url: null,
+					// }
+				],
 
 				// 社区基层人员注册需要的信息
 				name: '',
@@ -145,43 +150,44 @@
                		// defaultValue: ['河北省','唐山市','丰南区'],
                		column: 3
 				},
-				confirm (val) {
-            	    console.log(val);
-            	    this.city_picker.str = JSON.stringify(val);
-            	    this.city_picker.visible = false;
-            	},
-            	cancel () {
-            	    this.city_picker.visible = false;
-            	},
+
+				// 传给后端，拿到地址
+				action: this.$url.BASE_URL + '/4142061-0-default/schoolteam/pyq/createpost/uploadpics',
 			}
 		},
 		computed: {
 			identity(){
-				return index==0 ? 'team' : 'community' ;
+				return this.index==0 ? 'team' : 'community' ;
 			}
+		},
+		onShow(){
+			console.log('this.avatarList', this.avatarList, this.avatarList.length);
 		},
 		methods: {
 			register() {
+				console.log('注册');
 				/**
 				 * 客户端对账号信息进行一些必要的校验。
 				 * 实际开发中，根据业务需要进行处理，这里仅做示例。
 				 */
-				
-				if(this.index == 0){
+				if(this.index === 0){
+					console.log('高校队伍注册');
 					if(this.account == '' || this.team_name == '' || this.remark == ''){
 						uni.showToast({
+							icon: 'error',
 							title: '请填写完整信息！'
 						}); 
 						return ;
 					}
 					if(this.password != this.confirm_pwd){
 						uni.showToast({
+							icon: 'error',
 							title: '前后密码不一致！'
 						}); 
 						return ;
 					}
 					const data = {
-						identity: 'team',
+						identity: this.identity,
 						leader_id: this.account,
 						pwd: this.password,
 						team_name: this.team_name,
@@ -189,6 +195,7 @@
 						avatar: this.avatar
 
 					}
+					console.log('data',data)
 					uni.request({
 						// 高校
 						url: this.$url.BASE_URL + '/4142061-0-default/auth/register/schoolteam',
@@ -201,7 +208,7 @@
 									icon: 'success',
 									title: '注册成功，前往登录界面',
 								});
-								uni.reLaunch({
+								uni.navigateTo({
 									url:'../login/login'
 								});
 							}
@@ -215,7 +222,8 @@
 											uni.reLaunch({
 												url:'../login/login'
 											});
-								        } else if (res.cancel) {
+								        } 
+										else if (res.cancel) {
 								            console.log('取消去登录');
 								        }
 								    }
@@ -231,27 +239,64 @@
 					})
 				}
 
-				const data = {
-					
-					password: this.password,
-					email: this.email
-				}
-				service.addUser(data);
-				uni.showToast({
-					title: '注册成功'
-				});   
-				// uni.navigateBack({
-				// 	delta: 1
-				// });
-				setTimeout(function() {
-					uni.navigateBack({
-						delta: 1
-					});
-				}, 1000);  //延时解决不弹出的问题
 			},
 			open(){
 				this.city_picker.visible = true;
-			}
+			},
+			confirm (val) {
+                console.log(val);
+                this.city_picker.str = JSON.stringify(val);
+                this.city_picker.visible = false;
+            },
+            cancel () {
+                this.city_picker.visible = false;
+            },
+
+			// 上传成功时，赋值给avatar
+			handleSuccess(data, index, lists, index2){
+				console.log('上传图片成功，链接为',data);
+				console.log('index',index);
+				console.log('lists',lists);
+				console.log('index2',index2);
+				console.log('avatarList',this.avatarList);
+				if(index ===0 ){
+					this.avatar = data;
+				}
+				
+				
+			},
+			//删除时
+			handleRemove(index, lists, name){
+				console.log('删除图片',index,lists,name);
+				console.log('删除图片',index,lists,name);
+				this.avatar = '';
+				console.log('删除图片',index,lists,name);
+			},
+			change(){
+				console.log('转换身份',this.avatar);
+				console.log('转换身份',this.$refs.uUpload.lists);
+				this.account = '';
+				this.password= '';
+				this.confirm_pwd= '';
+				this.team_name= '';
+				this.setup_date= '';
+				this.remark= '';
+				this.leader_id= '';
+				this.avatar='';
+				this.avatarList= [
+					// {url: '',}
+				];
+
+				// 社区基层人员注册需要的信息
+				this.name= '';
+				this.tel= '';
+				this.province= '';
+				this.city= '';
+				this.address= '';
+				this.$refs.uUpload.clear();
+				console.log('转换身份',this.avatar);
+				console.log('转换身份',this.$refs.uUpload.lists);
+			},
 		}
 	}
 </script>
@@ -263,9 +308,17 @@
     width: 90px;
   }
   .banner {
-    margin: 0 auto;;
-    width: 50px;
-    height: 50px;
+    margin: 10rpx auto;;
+    width: 100rpx;
+    height: 100rpx;
+	align-items: center;
+	max-width: 100%;
+  	max-height: 100%;
+  }
+  .banner2 {
+    margin: 10rpx auto;;
+    width: 100rpx;
+    height: 100rpx;
 	align-items: center;
   }
   .sms {
@@ -286,5 +339,27 @@
 	background-image: url(http://scu5azomr.hn-bkt.clouddn.com/static/bg_img/11.png);
 	background-size: cover;
     background-attachment: fixed;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30rpx;
+}
+/* .slot-btn {
+	width: 329rpx;
+	height: 140rpx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: rgb(244, 245, 246);
+	border-radius: 10rpx;
+}
+
+.slot-btn__hover {
+	background-color: rgb(235, 236, 238);
+} */
+.noCSS{
+
 }
 </style>
