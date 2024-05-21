4a69572664983a387bb8bf689e2c34c7962ec362
GreyJoey <2530116785@qq.com>
2024-05-21 10:53:19 +0800

    包原畅-在后台打印请求

--- server/app.js
+++ server/app.js
@@ -1,30 +1,37 @@
+// 导入所需模块
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// 创建 Express 应用
 const app = express();
 
-const jwtUtils = require('./app/utils/jwtUtils');
-const jwtMiddleware = require('./app/middleware/jwtMiddleware');
+// 导入日志记录模块
+const morgan = require("morgan");
 
-var corsOptions = {
+// 设置 CORS 选项
+const corsOptions = {
   origin: "*"
 };
 
+// 使用 CORS 中间件
 app.use(cors(corsOptions));
 
-// content-type：application/json
+// 使用 bodyParser 解析 JSON 请求体
 app.use(bodyParser.json());
 
-// content-type：application/x-www-form-urlencoded
+// 使用 bodyParser 解析 URL 编码的请求体
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// 使用 Morgan 中间件记录请求信息
+app.use(morgan("dev"));
 
 // 简单路由
 app.get("/", (req, res) => {
   res.json({ message: "欢迎访问我们的后端服务器" });
 });
 
-// 获取路由
+// 导入路由模块
 const communityRouter = require('./app/routes/communityRouter');
 const authRouter = require('./app/routes/authRouter');
 const teamRouter = require('./app/routes/teamRouter');
@@ -32,16 +39,14 @@ const schoolRouter = require('./app/routes/schoolRouter');
 const imageRouter = require('./app/routes/imageRouter');
 
 // 使用路由
-app.use('/auth', authRouter)
-app.use('/community', communityRouter)
-app.use('/school',schoolRouter)
-app.use('/schoolteam',teamRouter)
-app.use('/uploadImage',imageRouter)
-
-
+app.use('/auth', authRouter);
+app.use('/community', communityRouter);
+app.use('/school', schoolRouter);
+app.use('/schoolteam', teamRouter);
+app.use('/uploadImage', imageRouter);
 
 // 设置监听端口
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`服务器运行端口： ${PORT}.`);
 });
--- server/app/routes/authRouter.js
+++ server/app/routes/authRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/register/community', authController.register);
 router.post('/register/schoolteam', authController.registerTeam);
+router.post('/register/', authController.register);
 
 router.post('/login', authController.login);
 router.post('/login/schoolteam', authController.loginTeam);
