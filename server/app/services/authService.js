330e840fd0973c0e4193a7a48e8b69084da4b324
GreyJoey <2530116785@qq.com>
2024-05-21 17:14:45 +0800

    包原畅-高校注册-修改完成

--- server/app/services/authService.js
+++ server/app/services/authService.js
@@ -95,47 +95,42 @@ class AuthService {
     };
 
     static async registerTeam(registerData) {
-        const team = { ...registerData };
-        delete team.identity;
-
-
-        const user = await db.teammember.findOne({
-            where: {
-                id: team.leader_id // 匹配用户ID
-            }
-        });
-
-        if (!user) {
-            throw new Error('队长不存在');
-        }
-
-        // loginData.pwd是用户输入的密码，user.password是从数据库中获取的哈希密码
-        const passwordMatch = await bcrypt.compare(team.pwd, user.pwd);
-
-
-        if (!passwordMatch) {
-            throw new Error('密码错误');
-        }
-
-
-
-        // 创建 short-uuid 实例
-        const uuid = short();
+        const t = await db.sequelize.transaction();
         
-        // 自动生成id，生成短UUID
-        const shortId = uuid.new();
-        
-
-        // 加密密码
-        team.pwd = await bcrypt.hash(team.pwd, 10);
-        team.id = shortId;
-
-        let model;
-        model = db.team;
         try {
-            await model.create(team);
-            return model;
+            const team = { ...registerData };
+            delete team.identity;
+    
+            // 创建 short-uuid 实例
+            const uuid = short();
+            
+            // 自动生成id，生成短UUID
+            const shortId = uuid.new();
+            
+            // 加密密码
+            team.pwd = await bcrypt.hash(team.pwd, 10);
+            team.id = shortId;
+            team.verification_status = 1;
+    
+            const newTeam = { ...team };
+            delete newTeam.pwd;
+    
+            const leader = await db.teammember.create({
+                id: team.leader_id,
+                team_id: team.id,
+                pwd: team.pwd,
+                transaction: t,
+            });
+    
+            const createdTeam = await db.team.create(newTeam, { transaction: t });
+    
+            await t.commit();
+    
+            return createdTeam;
         } catch (error) {
+            if (t) {
+                await t.rollback();
+            }
             throw new Error(error.message);
         }
     }
