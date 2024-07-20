9445cd8c74ceb313314b76f610c6b447bbd0797e
GreyJoey <2530116785@qq.com>
2024-07-20 22:46:12 +0800

    包原畅-增加结果码

--- server/app/common/BaseResultCode.js
+++ server/app/common/BaseResultCode.js
@@ -12,9 +12,9 @@ class BaseResultCode {
     /************************************/
     static SUCCESS = new BaseResultCode(200, 'success');
     static FAILED = new BaseResultCode(500, 'failed');
-    // static VALIDATE_FAILED = new BaseResultCode(400, '参数校验失败');
-    // static API_NOT_FOUNT = new BaseResultCode(404, '接口不存在');
-    // static API_BUSY = new BaseResultCode(429, '操作过于频繁')
+    static VALIDATE_FAILED = new BaseResultCode(400, '参数校验失败');
+    static API_NOT_FOUNT = new BaseResultCode(404, '接口不存在');
+    static API_BUSY = new BaseResultCode(429, '操作过于频繁')
 }
 
 module.exports = BaseResultCode
