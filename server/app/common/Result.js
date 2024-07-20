c8249cf628499cdfe6cb39b485f69f0255359b85
GreyJoey <2530116785@qq.com>
2024-07-20 23:33:18 +0800

    Add support for more result codes

--- server/app/common/Result.js
+++ server/app/common/Result.js
@@ -1,63 +1,30 @@
-const ResultCode = require('./BaseResultCode');
-/**
- * @author byc
- * @description 统一返回结果
- */
-class Result {
-    /**
-     * 返回code
-     */
-    code;
-    /**
-     * 返回消息
-     */
-    msg;
-    /**
-     * 返回数据
-     */
-    data;
+const BaseResultCode = require('./BaseResultCode');
 
-    /**
-     * 
-     * @param code {number} 返回code
-     * @param msg {string} 返回消息
-     * @param data {any} 返回具体对象
-     */
+class Result {
     constructor(code, msg, data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    /**
-     * 成功
-     * @param data {any} 返回对象
-     * @return {Result}
-     */
     static success(data) {
-        return new Result(ResultCode.SUCCESS.code, ResultCode.SUCCESS.desc, data);
+        const successCode = BaseResultCode.getCodeByKey('SUCCESS');
+        return new Result(successCode.code, successCode.desc, data);
     }
 
-    /**
-     * 失败
-     */
     static fail(errData) {
-        return new Result(ResultCode.FAILED.code, ResultCode.FAILED.desc, errData);
+        const failCode = BaseResultCode.getCodeByKey('FAILED');
+        return new Result(failCode.code, failCode.desc, errData);
     }
 
-    /**
-     * 参数校验失败
-     */
     static validateFailed(param) {
-        return new Result(ResultCode.VALIDATE_FAILED.code, ResultCode.VALIDATE_FAILED.desc, param);
+        const validateFailedCode = BaseResultCode.getCodeByKey('VALIDATE_FAILED');
+        return new Result(validateFailedCode.code, validateFailedCode.desc, param);
     }
 
-    /**
-     * 拦截到的业务异常
-     * @param bizException {BizException} 业务异常
-     */
     static bizFail(bizException) {
         return new Result(bizException.code, bizException.msg, null);
     }
 }
-module.exports = Result
+
+module.exports = Result;
